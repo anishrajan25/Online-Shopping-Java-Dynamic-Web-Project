dd93c67b64aa5693778332f6dc9ff4d9f2e2f1a9
parteek-3108 <parteekad03@gmail.com>
2020-12-19 16:36:06 +0530

    Data BAse file

--- src/com/onlineStore/util/DBUtil.java
+++ src/com/onlineStore/util/DBUtil.java
@@ -1,26 +1,31 @@
 package com.onlineStore.util;
 
-import java.sql.Connection;
+import java.sql.*;
 import java.sql.DriverManager;
 public class DBUtil {
 	 static Connection con;
-	 public static void main(String [] args)
-	 {
-		 DBUtil.getConnection();
-	 }
+	//	 public static void main(String [] args)
+//	 {
+//		 DBUtil.getConnection();
+//	 }
+	 
 	 public static Connection getConnection()
 	 {
+		 System.out.println(con==null);
 		 if(con==null)
 		 {
 		 try {
 			 Class.forName("oracle.jdbc.driver.OracleDriver");
 				con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","demodbjava","parteek@1234");
-			} 
+			
+		 } 
 			catch (Exception e) {
+				System.out.println(con==null);
 				System.out.println(e);
 			} 
 		 }
 		 System.out.println("done");
+		 System.out.println(con==null);
 			return con;
 	 }
 }
